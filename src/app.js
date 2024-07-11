cfd1116891d654e2cbec98189f7cf147ccea42a1
Tanmay Trivedi <tanmaytrivedi57@gmail.com>
2024-07-12 01:13:07 +0530

    updated app.js

--- src/app.js
+++ src/app.js
@@ -2,7 +2,6 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 require("dotenv").config();
-const cors = require("cors");
 const taskRoutes = require("./routes/taskRoutes");
 
 const app = express();
@@ -10,7 +9,6 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(bodyParser.json());
-app.use(cors());
 app.get('/' , (req , res)=>{
     res.send("Hello World")
 })
